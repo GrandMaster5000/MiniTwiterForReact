b6d96bcde767f02daf7afc22d0213fc1f084753e
Tolik22869 <ggamer556@mail.ru>
2021-08-12 16:10:15 +0300

    refactoring code on postListItem , add events important and like

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -7,6 +7,13 @@ import PostAddForm from '../post-add-form';
 import './app.css';
 
 const App = () => {
+
+    const data = [
+        {label: "Going to learn react", important: true, id: 'rere'},
+        {label: "That is so good", important: false, id: 'gdgd'},
+        {label: "I need a break...", important: false, id: 'vcx'}
+    ];
+
     return (
         <div className="app">
             <AppHeader/>
@@ -14,7 +21,7 @@ const App = () => {
                 <SearchPanel/>
                 <PostStatusFilter/>
             </div>
-            <PostList/>
+            <PostList posts={data}/>
             <PostAddForm/>
         </div>
     )
